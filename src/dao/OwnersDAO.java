e8befb0370d633bc70f16a1709c3c2f6ae45f252
Tomasz Staroń <tomaszstaron@icloud.com>
2018-12-25 11:07:57 +0100

    Hibernate config + mappings
    reverse engineering mappings,
    db connecion settings,
    test ok

---
+++ src/dao/OwnersDAO.java
@@ -0,0 +1,21 @@
+package dao;
+
+import entity.Owners;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class OwnersDAO 
+{
+    static Session session = null;
+    
+    static public void insertOwner(int pesel, String name, String surname, String address)
+    {
+        Owners data = new Owners(pesel, name, surname, address);
+        
+        session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(data);
+        tx.commit();
+        session.close();
+    }
+}
